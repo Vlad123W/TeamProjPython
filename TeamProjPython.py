8b3e9600466087e7a7ea75bb6269b3589e28e1f0
Admin <126456347+Vlad123W@users.noreply.github.com>
2024-08-30 23:44:24 +0300

    added some text for manipulations

--- TeamProjPython.py
+++ TeamProjPython.py
@@ -1,6 +1,24 @@
-print("Hello world!")
-print("Idi nah")
-print("!!!!!!");
 
-print("Idi nah");
-print("Sam idi")
+from itertools import count
+from operator import concat
+
+def findSomeLetter(target):
+    counter = 0;
+    for i in range(0, len(someText)):
+        if someText[i] == target:
+            counter += 1
+    return counter
+
+
+def getFullInfo(character):
+    return "{repitions} repitions. Unocode code {unicode}".format(repitions = findSomeLetter(character), unicode = ord(character))
+
+
+#Text for manipulations
+someText = ("English culture is a rich tapestry woven from centuries of history, tradition, and innovation. " 
+"Renowned for its contributions to literature, with iconic writers like Shakespeare and Austen, it has shaped global narratives. "
+"The arts, including music, theater, and visual arts, hold a prominent place, with institutions like the Royal Academy and the West End showcasing the nation's talent. "
+"English customs, such as afternoon tea and the love for cricket, reflect a unique blend of formality and leisure. "
+"The countryside, with its rolling hills and quaint villages, embodies the idyllic charm that has inspired poets and artists alike.")
+
+print(getFullInfo('i'))
