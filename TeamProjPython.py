1089d67d14d4b730aa06f1d223d9b63c2ff8d697
IvaNeighbor <duzak2006@gmail.com>
2024-10-09 13:18:30 +0300

    Duzhak Ivan KN-33

--- TeamProjPython.py
+++ TeamProjPython.py
@@ -1,52 +1,105 @@
+def Print(students):
+    for student in students:
+        print(f"Group number: {student['group_number']}")
+        print(f"Full name: {student['full_name']}")
+        print(f"Course: {student['course']}")
+        print("Subjects and grades:")
+        for subject, grade in student["subjects"].items():
+            print(f"  - {subject}: {grade}")
+        print()
 
-from itertools import count
-from operator import concat
-import string
+def add_student(students_list, new_student):
+    students_list.append(new_student)
+    Print(students)
 
-def findSomeLetter(target):
-    counter = 0;
-    for i in range(0, len(someText)):
-        if someText[i] == target:
-            counter += 1
-    return counter
+students = [
+    {
+        "group_number": "КН-33",
+        "full_name": "Baranov Erik Vitaliyovych",
+        "course": 3,
+        "subjects": {
+            "Algorithms and data structures": 85,
+            "MMDO": 90,
+            "Python programming": 88,
+            "Numerical methods": 75
+        }
+    },
+    {
+        "group_number": "КН-31",
+        "full_name": "Kuchma Petro Dorianovych",
+        "course": 1,
+        "subjects": {
+            "Algorithms and data structures": 75,
+            "MMDO": 51,
+            "Python programming": 93,
+            "Numerical methods": 66
+        }
+    },
+    {
+        "group_number": "КН-32",
+        "full_name": "Poroh Kate Mykolaivna",
+        "course": 2,
+        "subjects": {
+            "Algorithms and data structures": 77,
+            "MMDO": 98,
+            "Python programming": 78,
+            "Numerical methods": 85
+        }
+    },
+    {
+        "group_number": "КН-34",
+        "full_name": "Raven Arnold Adolfovich",
+        "course": 1,
+        "subjects": {
+            "Algorithms and data structures": 61,
+            "MMDO": 79,
+            "Python programming": 80,
+            "Numerical methods": 92
+        }
+    },
+    {
+        "group_number": "КН-35",
+        "full_name": "Stark Ermund Adamovych",
+        "course": 4,
+        "subjects": {
+            "Algorithms and data structures": 88,
+            "MMDO": 95,
+            "Python programming": 79,
+            "Numerical methods": 97
+        }
+    },
+ ]  
 
+new_student = {
+ "group_number": "КН-37",
+ "full_name": "Wise Olga Yaroslavivna",
+ "course": 3,
+ "subjects": {
+     "Algorithms and data structures": 91,
+     "MMDO": 95,
+     "Python programming": 97,
+     "Numerical methods": 99
+ }
+}
 
-def getFullInfo(character):
-    return "[Target: '{target}'; Repitions: {repitions}; Unicode: {unicode}]".format(target = character, repitions = findSomeLetter(character), unicode = ord(character))
+def case1():
+    return Print(students)
 
+def case2():
+    return add_student(students, new_student)
 
-#Text for manipulations
-someText = ("English culture is a rich tapestry woven from centuries of history, tradition, and innovation. " 
-"Renowned for its contributions to literature, with iconic writers like Shakespeare and Austen, it has shaped global narratives. "
-"The arts, including music, theater, and visual arts, hold a prominent place, with institutions like the Royal Academy and the West End showcasing the nation's talent. "
-"English customs, such as afternoon tea and the love for cricket, reflect a unique blend of formality and leisure. "
-"The countryside, with its rolling hills and quaint villages, embodies the idyllic charm that has inspired poets and artists alike.")
+def default():
+    return "Wrong number"
 
-print(getFullInfo('y'))
+switch = {
+    1: case1,
+    2: case2,
+}
 
-#       ↑
-#Konoplenko KN-33-1
-
-result=string.capwords(someText)
-print(result)
-print("-----")
-print(someText.isdigit())
-print("-----")
-result=someText.replace('English', 'Ukraine')
-print(result)
+print("Enter a number 1, if you want print of vocabulary;")
+print("Enter a number 2, if you want to add new student;")
+option = int(input("Enter a number (1-2): "))
+switch.get(option, default)()
 
 #       ↑
 #Duzhak Ivan KN-33-1
-
-someText = someText.lower()
-someText = someText.replace(',', ' ').replace('.', ' ')
-print("\n")
-print(someText)
-print("\n")
-words = someText.split()
-unique_words = set(words)
-print(f"Загальна кількість слів: {len(words)}")
-print(f"Кількість унікальних слів: {len(unique_words)}")
-
-#       ↑
-#Konyk Vlad KN-31-1
